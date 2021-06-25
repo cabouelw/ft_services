2e82a09052ce179b0ae008592879d3a782c7ea5b
Choaib Abouelwafa <cabouelw@e2r1p4.1337.ma>
2021-06-25 14:12:32 +0100

    fix proxy & ....

--- setup.sh
+++ setup.sh
@@ -1,19 +1,21 @@
+minikube config set memory 4144
+minikube config set cpus 4
 minikube start
 eval $(minikube docker-env)
-docker build -t nginx ./nginx
-docker build -t phpmyadmin ./phpmyadmin
-docker build -t wordpress ./wordpress
-docker build -t mysql ./mysql
-docker build -t influxdb  ./influxdb
-docker build -t grafana ./grafana
-docker build -t ftps ./ftps
+docker build -t nginx ./srcs/nginx
+docker build -t phpmyadmin ./srcs/phpmyadmin
+docker build -t wordpress ./srcs/wordpress
+docker build -t mysql ./srcs/mysql
+docker build -t influxdb  ./srcs/influxdb
+docker build -t grafana ./srcs/grafana
+docker build -t ftps ./srcs/ftps
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.10.2/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.10.2/manifests/metallb.yaml
-kubectl apply -f ./ymlFiles/nginx.yaml
-kubectl apply -f ./ymlFiles/mysql.yaml
-kubectl apply -f ./ymlFiles/wordpress.yaml
-kubectl apply -f ./ymlFiles/phpmyadmin.yaml
-kubectl apply -f ./ymlFiles/influxdb.yaml
-kubectl apply -f ./ymlFiles/grafana.yaml
-kubectl apply -f ./ymlFiles/ftps.yaml
-kubectl apply -f ./ymlFiles/metallb.yaml
+kubectl apply -f ./srcs/ymlFiles/nginx.yaml
+kubectl apply -f ./srcs/ymlFiles/mysql.yaml
+kubectl apply -f ./srcs/ymlFiles/wordpress.yaml
+kubectl apply -f ./srcs/ymlFiles/phpmyadmin.yaml
+kubectl apply -f ./srcs/ymlFiles/influxdb.yaml
+kubectl apply -f ./srcs/ymlFiles/grafana.yaml
+kubectl apply -f ./srcs/ymlFiles/ftps.yaml
+kubectl apply -f ./srcs/ymlFiles/metallb.yaml
--- srcs/FTPS/srcs/run.sh
+++ srcs/FTPS/srcs/run.sh
@@ -1,7 +1,15 @@
+
+addgroup -g 433 -S 1337
+adduser -u 431 -D -G 1337 -h /home/1337 -s /bin/false 1337
+
+echo "1337:1337" | chpasswd
+chown 1337:1337 /home/1337/ -R
+
 telegraf &
 rc-service vsftpd start
 
 sleep 2
+
 
 while true;
 do
